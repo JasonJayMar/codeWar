be47489d1e8663230110df5e152010a7aae8df11
Jason Martinez <39502339+JasonJayMar@users.noreply.github.com>
2018-07-11 22:08:18 -0400

    Add files via upload

---
+++ codeWar13/js/main.js
@@ -0,0 +1,13 @@
+function stray(array){
+  debugger;
+  for(var i = 0; i < array.length; i++){
+    if(array[i] === array[i+1]){
+      if(array[i] === array[i+2]){
+      } else {
+        return array[i+2];
+      }
+    } else if(array[i] !== array[i+2]){
+      return array[i]
+    }
+  }
+}
